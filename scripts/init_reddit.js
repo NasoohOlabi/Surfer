6cc925891fcc0512baf38068dff3fc486312ca18
Nasooh Olabi <nasooholabi@gmail.com>
2023-04-22 16:10:10 +0300

    reddit scripts

--- scripts/init_reddit.js
+++ scripts/init_reddit.js
@@ -1,5 +1,12 @@
-window.curser = [].slice.call(document.querySelectorAll(`div[data-scroller-first]`));
-curser = curser.filter(x => x.querySelector('i.icon-upvote'))[0] || curser[0];
-let x = curser.querySelector(`#0`);
-console.log(x);
-return x.textContent;
+/* window.curser = [].slice.call(document.querySelectorAll(`div[data-scroller-first]`));
+curser = curser.filter(x => x.querySelector('i.icon-upvote'))[0] || curser[0]; */
+window.curser = null;
+
+const intervalId = setInterval(() => {
+	const elements = document.querySelectorAll(`div[data-scroller-first]`);
+	const filteredElements = Array.from(elements).filter(x => x.querySelector('i.icon-upvote'));
+	curser = filteredElements.length > 0 ? filteredElements[0] : elements[0];
+	if (curser) {
+		clearInterval(intervalId);
+	}
+}, 500);
--- scripts/reddit next.js
+++
@@ -1,6 +0,0 @@
-console.log(`curser : `);
-console.log(curser);
-console.log(`window.curser : `);
-console.log(window.curser);
-curser = curser.nextSibling;
-return curser.textContent;
